1b8041ac6a04c0948f9fbaeb2b1e59d591e8c90a
sprinkuls <maymacler@gmail.com>
2025-05-29 17:28:12 -0400

    add bv file parsing

--- stateManager.js
+++ stateManager.js
@@ -22,6 +22,7 @@ const globalState = {
     light1: true,
     light2: false,
     light3: false,
+    boundingBox: false,
     patchDetail: '4 x 4',
 
     // Help
@@ -53,7 +54,6 @@ const globalState = {
 
             // Display
             controlMesh: false,
-            boundingBox: false,
             showPatches: true,
 
             // Groups
@@ -124,11 +124,10 @@ function setupGui()
     // 'Display' Section
     const displayFolder = gui.addFolder('Display').close();
     const controlMesh = displayFolder.add(globalState.groupSettings["All Groups"], 'controlMesh').name('Show Control Mesh').onChange(propogateState('controlMesh'));
-    const boundingBox = displayFolder.add(globalState.groupSettings["All Groups"], 'boundingBox').name('Show Bounding Box').onChange(propogateState('boundingBox'));
+    displayFolder.add(globalState, 'boundingBox').name('Show Bounding Box');
     const showPatches = displayFolder.add(globalState.groupSettings["All Groups"], 'showPatches').name('Show Patches').onChange(propogateState('showPatches'));
 
     controllers.push(controlMesh);
-    controllers.push(boundingBox);
     controllers.push(showPatches);
 
     displayFolder.add(globalState, 'light1').name('Light 1');
@@ -149,7 +148,7 @@ function setupGui()
     const groupColor = groupsFolder.add(globalState.groupSettings["All Groups"], 'groupColor', groupColorOptions).name('Group Color').onChange(propogateState('groupColor'));
     controllers.push(groupColor);
 
-    groupsFolder.add({func() {addGroup();}}, 'func').name('Add Group');
+    //groupsFolder.add({func() {addGroup();}}, 'func').name('Add Group');
     groupsFolder.add({func() {}}, 'func').name('Delete Group');
 
 
@@ -200,37 +199,242 @@ function propogateState(settingsKey){
     return callback;
 }
 
-// add a new group with the settings currently in 'All Groups'
-function addGroup() {
-    const numGroups = Object.keys(globalState.groupSettings).length;
-    const groupName = `Group ${numGroups}`;
-
-    globalState.groupSettings[groupName] = structuredClone(globalState.groupSettings["All Groups"]);
-
-    // make this new group the active one
-    globalState.activeGroup = groupName;
-    groupHandle.setValue(groupName);
-    changeGroup(groupName); // necessary as setValue() doesn't trigger onChange()
-
-    // update the group options to include the new group
-    groupHandle.options(Object.keys(globalState.groupSettings));
-}
-
-
+// Load the file, parse the file, update the global state, update the GUI
 function loadFile(file) {
-    // Load the file, parse the file, update the global state, update the GUI
-    if(file){
+
+    if (file) {
         const reader = new FileReader();
         reader.onload = function(event) {
-            const fileContent = event.target.result;
-            // Parse the file content here
-            // Update globalState with the parsed data
-            // Call updateGui() to refresh the GUI with new data
-            // Only override state if the entire file is successfully parsed
-            alert(fileContent);
+
+            // local copy of group settings, so that global state isn't changed prematurely
+            let groupSettingsLocal = {};
+
+            // only copy in 'All Groups' since we if we're loading a new model, we don't
+            // want to keep around groups from another file
+            groupSettingsLocal["All Groups"] = structuredClone(globalState.groupSettings["All Groups"]);
+
+            // split the input into whitespace-delimited tokens
+            const input = (event.target.result).split(/\s+/);
+
+            // index in the 'input' array
+            let idx = 0;
+
+            // utility to read groups of data from the file
+            // returns an array of [x, y, z, w] points, where w is the 'rational'
+            // value that acts as a weight for a control point
+            function readPoints(numPoints, isRational) {
+                let points = [];
+                // if the patch is rational, we need to read a 4th value
+                if (isRational) {
+                    for (let i = 0; i < numPoints; i++) {
+                        let p = [];
+                        p.push(Number(input[idx++]));
+                        p.push(Number(input[idx++]));
+                        p.push(Number(input[idx++]));
+                        p.push(Number(input[idx++]));
+                        points.push(p);
+                    }
+                // if the patch isn't rational, make every 4th value 1
+                } else {
+                    for (let i = 0; i < numPoints; i++) {
+                        let p = [];
+                        p.push(Number(input[idx++]));
+                        p.push(Number(input[idx++]));
+                        p.push(Number(input[idx++]));
+                        p.push(1);
+                        points.push(p);
+                    }
+                }
+                return points;
+            }
+
+
+            // create a default group in case none are specified in the file
+            groupSettingsLocal['(unnamed group)'] = structuredClone(groupSettingsLocal["All Groups"]);
+            groupSettingsLocal['(unnamed group)'].patches = [];
+
+            let currentGroup = groupSettingsLocal['(unnamed group)'];
+
+            // loop for every token in the input file until we reach EOF
+            while (idx < input.length && input[idx] != '') {
+                // this 'if' statement runs when we run into the start of a group.
+                // it first parses the group information (what ID the group is,
+                // and what name it has), then it checks to see if another group
+                // already exists with this information. if one does exist,
+                // then we're just adding more data to that group. if one doesn't
+                // exist, we need to create a new group.
+                if (input[idx].toUpperCase() == "GROUP") {
+                    let groupID = Number(input[++idx]);
+                    let groupName = input[++idx];
+                    console.log("group with ID", groupID, "and name", groupName);
+
+                    // if this group doesn't exist, create it
+                    if (!Object.hasOwn(groupSettingsLocal, groupName)) {
+                        groupSettingsLocal[groupName] = structuredClone(groupSettingsLocal["All Groups"]);
+                        groupSettingsLocal[groupName].patches = [];
+                    }
+
+                    // set this group as the current one
+                    currentGroup = groupSettingsLocal[groupName];
+                    idx++;
+                }
+
+                // whether we just read a group label or didn't, we should now
+                // be coming up on the start of a patch, the first part of which
+                // will be a number describing the patch type
+                let patchType = Number(input[idx++]);
+
+                switch (patchType) {
+                    // polyhedron
+                    case 1: {
+                        let patch = {};
+                        patch.patchType = patchType;
+                        patch.deg_u = 0;
+                        patch.deg_v = 0;
+
+                        let numVertices = Number(input[idx++]);
+                        let numFaces = Number(input[idx++]);
+
+                        // load vertices
+                        patch.mesh = readPoints(numVertices, false);
+                        // load faces/edges
+                        let faces = [];
+                        let edges = [];
+                        for (let i = 0; i < numFaces; i++) {
+                            var faceSize = Number(input[idx++]);
+                            var verts = [];
+                            for (var j = 0; j < faceSize; j++) {
+                                verts.push(Number(input[idx++]));
+                            }
+                            for (var j = 0; j < faceSize-2; j++) {
+                                faces.push(verts[0]);
+                                faces.push(verts[j+1]);
+                                faces.push(verts[j+2]);
+                            }
+                            for (var j = 0; j < faceSize; j++) {
+                                edges.push(verts[j]);
+                                edges.push(verts[(j+1)%faceSize]);
+                            }
+                        }
+                        patch.polyhedral_indices = faces;
+                        patch.polyhedral_edges = edges;
+
+                        console.log(patch.polyhedral_indices);
+                        currentGroup.patches.push(patch);
+
+                        break;
+                    }
+                    // triangular patch
+                    case 3: // not rational
+                    case 11: { // rational
+                        let patch = {};
+                        patch.patchType = patchType;
+
+                        let deg = Number(input[idx++]);
+                        patch.deg_u = deg;
+                        patch.deg_v = deg;
+
+                        let numPoints = (deg+2)*(deg+1)/2;
+                        patch.mesh = readPoints(numPoints, patchType === 11);
+                        currentGroup.patches.push(patch);
+
+                        break;
+                    }
+                    // tensor products
+                    case 4: // square (deg_u == deg_v)
+                    case 5: // quad (deg_u independent of deg_v)
+                    case 8: { // rational, 4th value to read
+                        let patch = {};
+                        patch.patchType = patchType;
+
+                        let deg_u, deg_v;
+                        if (patchType == 4) {
+                            patch.deg_u = deg_u = Number(input[idx]);
+                            patch.deg_v = deg_v = Number(input[idx++]);
+                        } else {
+                            patch.deg_u = deg_u = Number(input[idx++]);
+                            patch.deg_v = deg_v = Number(input[idx++]);
+                        }
+                        let numPoints = (deg_u + 1) * (deg_v + 1);
+                        patch.mesh = readPoints(numPoints, patchType === 8);
+                        currentGroup.patches.push(patch);
+
+                        break;
+                    }
+                    // PN triangles
+                    case 9: {
+                        let patch = {};
+                        patch.patchType = patchType;
+
+                        let deg = Number(input[idx++]);
+                        patch.deg_u = deg;
+                        patch.deg_v = deg;
+
+                        let normal_deg = Number(input[idx++]);
+
+                        let numPoints = ((deg + 2) * (deg + 1)) / 2;
+                        let numNormals = ((normal_deg + 2) * (normal_deg + 1)) / 2;
+
+                        patch.mesh = readPoints(numPoints, false);
+                        patch.artificial_normals = {
+                            deg_u: normal_deg,
+                            deg_v: normal_deg,
+                            normals: readPoints(numNormals, false)
+                        }
+                        currentGroup.patches.push(patch);
+
+                        break;
+                    }
+                    // PN quads
+                    case 10: {
+                        let patch = {};
+                        patch.patchType = patchType;
+
+                        let deg_u, deg_v;
+                        let n_deg_u, n_deg_v;
+                        patch.deg_u = deg_u = Number(input[idx++]);
+                        patch.deg_v = deg_v = Number(input[idx++]);
+                        n_deg_u = Number(input[idx++]);
+                        n_deg_v = Number(input[idx++]);
+
+                        let numPoints = (deg_u + 1) * (deg_v + 1);
+                        let numNormals = (normal_degu + 1) * (normal_degv + 1);
+
+                        patch.mesh = readPoints(numPoints, false);
+                        patch.artificial_normals = {
+                            deg_u: n_deg_u,
+                            deg_v: n_deg_v,
+                            normals: readPoints(numNormals, false)
+                        }
+                        currentGroup.patches.push(patch);
+
+                        break;
+                    }
+                    default: {
+                        throw 'invalid patch type: ' + patchType.toString();
+                    }
+                }
+            }
+
+            // if no data got stored in the default, unnamed group, remove it
+            if (groupSettingsLocal['(unnamed group)'].patches.length === 0) {
+                delete groupSettingsLocal['(unnamed group)'];
+            }
+
+            // only get to this point if we don't throw an error somewhere else;
+            // thus, it should now be safe to change the global state
+
+            // make 'All Groups' the currently set group (since the user might
+            // have a group selected that doesn't exist in this new model)
+            globalState.activeGroup = 'All Groups';
+            groupHandle.setValue('All Groups');
+            changeGroup('All Groups'); // necessary as setValue() doesn't trigger onChange()
+
+            // copy in the groups and update the group options
+            globalState.groupSettings = structuredClone(groupSettingsLocal);
+            groupHandle.options(Object.keys(globalState.groupSettings));
         }
         reader.readAsText(file);
-        
     }
 }
 
